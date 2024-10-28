6aef4467d835a913d436459093cae457967e81bc
E7ONMUSK <tanguygoursaud@gmail.com>
2024-10-28 10:25:17 +0100

    tp5 dev

---
+++ README.md
@@ -0,0 +1,29 @@
+# TP5 DEV : Coding Encoding Decoding
+
+## I. Jouer avec l'encodage
+
+### II. Opti calculatrice
+
+### 0. Setup
+
+### 1. Optimisation avec headers
+
+### 2. Optimisation avec taille fixe et connue
+
+### A. Python et les octets
+
+### B. Un autre exemple d'opti
+
+### C. Calculatrice opti
+
+### III. Serveur Web HTTP
+
+### 1. Serveur Web
+
+### 2. Client Web
+
+### 3. Délivrer des pages web
+
+### 4. Quelques logs
+
+### 5. File download
---
+++ tp5_enc_client_1.py
@@ -0,0 +1,33 @@
+import socket
+import re
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(('127.0.0.1', 9999))
+
+def is_calcul(value: str):
+    return re.search(r'^(-?\d+)\s*[\+\-\*]\s*(-?\d+)$', value)
+
+def check_byte_limit(l: list):
+    return 0 == len([int(x) for x in l if abs(int(x)) >= 1048575])
+
+msg = input("Calcul à envoyer: ")
+if not is_calcul(msg):
+    raise ValueError("Mauvais calcul")
+
+values = re.split(r"\s*[\+\-\*]\s*", msg)
+if not check_byte_limit(values):
+    raise ValueError("Valeur trop grande")
+
+msg_len = len(msg)
+END = 0
+encoded_msg = msg.encode('utf-8')
+
+header = msg_len.to_bytes(4, byteorder='big')
+footer = END.to_bytes(4, byteorder='big')
+payload = header + encoded_msg + footer
+
+s.send(payload)
+
+s_data = s.recv(1024)
+print(s_data.decode())
+s.close()
